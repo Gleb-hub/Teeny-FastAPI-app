4f0bf166e75d92445df557bfd7afe3a8ae212f89
gl_epka <nechaevgleb@inbox.ru>
2024-02-06 14:19:19 +0700

    included explorer route

--- src/web/main.py
+++ src/web/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+import explorer
 
 app = FastAPI()
+app.include_router(explorer.router)
 
 
 @app.get('/')
 def top():
     return 'top here'
+
+
+@app.get('/echo/{thing}')
+def echo(thing):
+    return f'echoing {thing}'
 
 
 if __name__ == '__main__':
